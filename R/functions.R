734fd382f598c594bc7d9de368c53c46a0ae1c34
Jared Knowles <jknowles@gmail.com>
2016-11-21 01:01:23 -0500

    almost done with collegegoing

--- R/functions.R
+++ R/functions.R
@@ -17,6 +17,31 @@ nvals <- function(x){
   length(unique(x))
 }
 
+# Cluster standard errors
+get_CL_vcov <- function(model, cluster){
+  # cluster is an actual vector of clusters from data passed to model
+  # from: http://rforpublichealth.blogspot.com/2014/10/easy-clustered-standard-errors-in-r.html
+  require(sandwich, quietly = TRUE)
+  require(lmtest, quietly = TRUE)
+  
+  # NA
+  cluster <- as.character(cluster)
+  
+  #calculate degree of freedom adjustment
+  M <- length(unique(cluster))
+  N <- length(cluster)
+  K <- model$rank
+  dfc <- (M/(M-1))*((N-1)/(N-K))
+  
+  #calculate the uj's
+  uj  <- apply(estfun(model), 2, function(x) tapply(x, cluster, sum))
+  
+  #use sandwich to get the var-covar matrix
+  vcovCL <- dfc*sandwich(model, meat=crossprod(uj)/N)
+  return(vcovCL)
+}
+
+
 
 # Assign one value
 # Task 1
